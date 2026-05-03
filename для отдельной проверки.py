2efecf014ec119a07a914ea669c942809811fc18
alex <saneck-1985@yandex.ru>
2026-05-03 18:53:14 +0300

    от 2 мая

--- для отдельной проверки.py
+++ для отдельной проверки.py
@@ -1,7 +1,4 @@
-# что такое глубокая копия ( deep copy ) в отличии от обычной которая .pop )
-import copy
-original_list = [1, 2, 3, ["a", "b", "c"]]
-copy_of_original_list = copy.deepcopy(original_list) # Создает полную копию оригинального списка со всеми элементами ( в этом и разница с обычной копией)
-original_list[3] = 4                                 # меняем значение но так как мы выполнили дипкопию то у нас теперь и оригинал сохранился и ихменения
-print(original_list)
-print(copy_of_original_list)
+cars = ["мерседес", "ауди", "бмв", "ауди", "ауди"]
+result = cars.pop(0)
+print(cars)
+print(result)
