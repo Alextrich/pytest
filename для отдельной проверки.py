0cfcb55c2897c79d059628d13ca4ad97a0e2927c
alex <saneck-1985@yandex.ru>
2026-05-10 18:11:13 +0300

    от 9 мая

--- для отдельной проверки.py
+++ для отдельной проверки.py
@@ -1,3 +1,14 @@
-num = 41
-result = "четное" if num % 2 == 0 else " Нечетное"
-print(result)
+# Как вывести ответ в JSON а не в строку
+import json                                                         # импортируем библиотеку json как в примере
+
+human = {
+    "name": "Alex",
+    "age": 40,
+    "education": False,
+    "list_of_items": ["математика", "физика", "программирование"],
+    "country": {
+        "Bulgary":"born",
+        "Russia": "live"
+    }
+}
+print(json.dumps(human, ensure_ascii=False, indent=4))             # выводим все как указано и 2 это команда для кириллицы и последняя - табуляция = 4 пробела
